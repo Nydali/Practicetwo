e0746e973d35e1d5f41cf95666967a48b8c05ff9
Nydali <Saints2812@gmail.com>
2024-05-15 11:37:30 +0200

    Update

---
+++ practicetwo/src/components/MyComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h2>This is my component</h2>
+      <p>This is some filler text for my component that is simply supposed to be as long as possible, therefore there is no reason to really read it.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
